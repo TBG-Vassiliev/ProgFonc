3daf8f389c1adea36a5f987eac68c433500cc25c
Vassiliev <108366642+TBG-Vassiliev@users.noreply.github.com>
2023-10-21 14:43:32 +0200

    Ajout des update de Romain à la branche Develop (#1) (#2)
    * Update Formula.hs
    
    * Update Literal.hs
    
    * Update NormalForm.hs
    
    * Update Main.hs
    
    * Update Spec.hs
    
    ---------
    
    Co-authored-by: Romtaug <87935987+Romtaug@users.noreply.github.com>

--- src/NormalForm.hs
+++ src/NormalForm.hs
@@ -1,37 +1,73 @@
-{- |
-  Module : NormalForm
-  Description : A module representing conjunctive normal forms
-  Maintainer  : ???
--}
-module NormalForm(CNF(..), size, toFormula, fromFormula, robinson) where
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Le module 'NormalForm' offre des fonctionnalités pour manipuler les formules logiques
+-- sous la forme normale conjonctive (CNF), y compris la conversion depuis et vers les formules logiques,
+-- vérifier si une formule est en CNF, et calculer la taille d'une CNF.
+module NormalForm (
+    CNF(..), 
+    toFormula, 
+    fromFormula, 
+    size, 
+    isCNF,
+    robinson  -- Ceci est un emplacement réservé pour une fonction future.
+) where
 
 import Data.Set (Set)
+import qualified Data.Set as Set
+import Formula (Formula(..)) -- Importe le type de données 'Formula'.
+import Literal (Literal(..), neg) -- Importe le type de données 'Literal' et la fonction 'neg'.
 
-import Formula (Formula)
-import Literal (Literal)
+-- | 'CNF' représente une forme normale conjonctive, qui est une conjonction de disjonctions de littéraux.
+newtype CNF = CNF (Set (Set Literal)) deriving (Eq, Show) 
 
--- | A conjunctive normal form
-newtype CNF = CNF (Set (Set Literal))
---  deriving Eq
--- !!! Once type 'Literal' derive typeclass 'Eq', implementation MUST derive typeclass 'Eq' so the previous line of code will have to be uncommented.
--- NB: 'newtype' is an optimized declaration of 'data' for types with a sole constructor with a sole parameter (i.e. encapsulation).
-
-instance Show CNF where
-  show _ = undefined -- TODO
-
--- | Size (number of literals)
-size :: CNF -> Int
-size _ = undefined -- TODO
-
--- | Convert normal form to logical formula
+-- | 'toFormula' convertit une CNF en une formule logique.
 toFormula :: CNF -> Formula
-toFormula _ = undefined -- TODO
+toFormula (CNF sets) = And [Or [literalToFormula lit | lit <- Set.toList clause] | clause <- Set.toList sets]
+  where
+    literalToFormula :: Literal -> Formula
+    literalToFormula (Pos s) = Var s
+    literalToFormula (Neg s) = Not (Var s)
 
--- | Convert logical formula to normal form
-fromFormula :: Formula -> CNF
-fromFormula _ = undefined -- TODO
+-- | 'fromFormula' tente de convertir une formule logique en CNF, en supposant que la formule d'entrée est déjà en CNF.
+fromFormula :: Formula -> Either String CNF
+fromFormula formula =
+    if isCNF formula
+    then Right . CNF . Set.fromList . map (Set.fromList . map formulaToLiteral . getLiterals) $ getClauses formula
+    else Left "La formule n'est pas en CNF."
+  where
+    getClauses, getLiterals :: Formula -> [Formula]
+    getClauses (And clauses) = clauses
+    getClauses _ = []
 
--- | Apply ROBINSON's rule on clauses
+    getLiterals (Or lits) = lits
+    getLiterals _ = []
+
+    formulaToLiteral :: Formula -> Literal
+    formulaToLiteral (Var s) = Pos s
+    formulaToLiteral (Not (Var s)) = Neg s
+    formulaToLiteral _ = error "Type de formule inattendu."
+
+-- | 'isCNF' vérifie si une formule est en CNF.
+isCNF :: Formula -> Bool
+isCNF (And clauses) = all isClause clauses
+isCNF _ = False
+
+-- | 'isClause' vérifie si une formule est une clause valide dans une CNF.
+isClause :: Formula -> Bool
+isClause (Or literals) = all isLiteral literals
+isClause _ = False
+
+-- | 'isLiteral' vérifie si une formule est un littéral valide.
+isLiteral :: Formula -> Bool
+isLiteral (Var _) = True
+isLiteral (Not (Var _)) = True
+isLiteral _ = False
+
+-- | 'size' retourne le nombre total de littéraux dans une CNF.
+size :: CNF -> Int
+size (CNF sets) = sum (map Set.size (Set.toList sets))
+
+-- | 'robinson' est un emplacement réservé pour l'algorithme d'unification de Robinson.
+-- Cela devrait être remplacé par une implémentation réelle de l'algorithme.
 robinson :: CNF -> CNF
-robinson _ = undefined -- TODO
-
+robinson = error "Non implémenté : l'algorithme de Robinson nécessite une logique spécifique."
