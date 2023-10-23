6096de7387d98eae8f9e6f4fb6fad2e24e34e702
Romtaug <87935987+Romtaug@users.noreply.github.com>
2023-10-23 15:09:08 +0200

    Update Formula.hs

--- src/Formula.hs
+++ src/Formula.hs
@@ -4,21 +4,21 @@ module Formula
   ( Formula(..),
     Environment,
     evaluate,
-    (<=>),
     simplify,
     isTautology,
     isContradiction,
-    toCNF
+    getVars,
+    (<=>)  -- Équivalence logique
   ) where
 
+import Control.Applicative (liftA2)
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Map (Map)
 import qualified Data.Map as Map
 import GHC.Generics (Generic)
-import Control.Applicative (liftA2)
 
--- `Formula` est un type de données qui représente une formule logique.
+-- `Formula` représente une formule logique.
 data Formula
   = Constant Bool                -- Constante booléenne : True ou False
   | Var String                   -- Variable propositionnelle
@@ -36,33 +36,53 @@ type Environment = Map String Bool
 evaluate :: Environment -> Formula -> Maybe Bool
 evaluate env formula = 
   case formula of
-    Constant b -> Just b  -- Une constante est évaluée comme sa propre valeur.
-    Var x      -> Map.lookup x env  -- Une variable prend la valeur dans l'environnement.
-    Not f      -> not <$> evaluate env f  -- La négation inverse la valeur de la formule.
-    And f1 f2  -> liftA2 (&&) (evaluate env f1) (evaluate env f2)  -- L'opérateur "et" logique.
-    Or f1 f2   -> liftA2 (||) (evaluate env f1) (evaluate env f2)  -- L'opérateur "ou" logique.
-    Implies f1 f2 -> liftA2 (<=) (fmap not (evaluate env f1)) (evaluate env f2)  -- L'implication logique.
-    Equivalent f1 f2 -> (==) <$> evaluate env f1 <*> evaluate env f2  -- L'équivalence logique.
+    Constant b -> Just b
+    Var x      -> Map.lookup x env
+    Not f      -> not <$> evaluate env f
+    And f1 f2  -> liftA2 (&&) (evaluate env f1) (evaluate env f2)
+    Or f1 f2   -> liftA2 (||) (evaluate env f1) (evaluate env f2)
+    Implies f1 f2 -> liftA2 (<=) (fmap not (evaluate env f1)) (evaluate env f2)
+    Equivalent f1 f2 -> (==) <$> evaluate env f1 <*> evaluate env f2
 
--- `simplify` simplifie une formule logique en appliquant des règles de simplification de base.
+-- `simplify` simplifie une formule logique.
 simplify :: Formula -> Formula
--- Le code ici applique des transformations spécifiques pour simplifier les formules.
+simplify formula = 
+  case formula of
+    Not (Not f) -> simplify f
+    And f1 f2   -> And (simplify f1) (simplify f2)
+    Or f1 f2    -> Or (simplify f1) (simplify f2)
+    -- autres cas de simplification ici
+    _           -> formula  -- cas par défaut, aucune simplification
 
--- `isTautology` vérifie si une formule est une tautologie (toujours vraie).
+-- `getVars` extrait toutes les variables uniques d'une formule.
+getVars :: Formula -> Set String
+getVars formula =
+  case formula of
+    Var x      -> Set.singleton x
+    Not f      -> getVars f
+    And f1 f2  -> getVars f1 `Set.union` getVars f2
+    Or f1 f2   -> getVars f1 `Set.union` getVars f2
+    Implies f1 f2 -> getVars f1 `Set.union` getVars f2
+    Equivalent f1 f2 -> getVars f1 `Set.union` getVars f2
+    _          -> Set.empty  -- cas par défaut, aucune variable
+
+-- `isTautology` vérifie si une formule est une tautologie.
 isTautology :: Formula -> Bool
-isTautology f = all (\env -> evaluate env f == Just True) (generateEnvironments f)
+isTautology formula = all (\env -> evaluate env formula == Just True) possibleEnvs
+  where
+    vars = getVars formula
+    possibleEnvs = map Map.fromList $ sequence [[(var, b) | b <- [True, False]] | var <- Set.toList vars]
 
--- `isContradiction` vérifie si une formule est une contradiction (toujours fausse).
+-- `isContradiction` vérifie si une formule est une contradiction.
 isContradiction :: Formula -> Bool
-isContradiction f = all (\env -> evaluate env f == Just False) (generateEnvironments f)
+isContradiction formula = all (\env -> evaluate env formula == Just False) possibleEnvs
+  where
+    vars = getVars formula
+    possibleEnvs = map Map.fromList $ sequence [[(var, b) | b <- [True, False]] | var <- Set.toList vars]
 
--- `toCNF` convertit une formule en forme normale conjonctive.
-toCNF :: Formula -> Formula
--- Le code ici transforme d'abord la formule en sa forme normale négative, puis en FNC.
-
--- D'autres fonctions auxiliaires, comme `eliminateImplications`, `toNNF`, `nnfToCNF`, etc., aident dans ces transformations.
--- La fonction `generateEnvironments` génère tous les environnements possibles (valuations) pour une formule donnée, en attribuant toutes les combinaisons possibles de vrai/faux aux variables de la formule.
-
--- L'opérateur `<=>` vérifie si deux formules sont logiquement équivalentes, c'est-à-dire qu'elles ont les mêmes valeurs de vérité dans tous les environnements possibles.
+-- L'opérateur `<=>` vérifie si deux formules sont équivalentes.
 (<=>) :: Formula -> Formula -> Bool
-f1 <=> f2 = all (\env -> evaluate env f1 == evaluate env f2) (generateEnvironments (Or f1 f2))
+f1 <=> f2 = all (\env -> evaluate env f1 == evaluate env f2) possibleEnvs
+  where
+    vars = getVars f1 `Set.union` getVars f2
+    possibleEnvs = map Map.fromList $ sequence [[(var, b) | b <- [True, False]] | var <- Set.toList vars]
