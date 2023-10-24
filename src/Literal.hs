abc2f5c414e9da8d79008915d26a5b45fbc1cebb
Romtaug <87935987+Romtaug@users.noreply.github.com>
2023-10-24 15:40:07 +0200

    Update Literal.hs

--- src/Literal.hs
+++ src/Literal.hs
@@ -1,24 +1,31 @@
 {-# LANGUAGE GADTs, KindSignatures, DeriveGeneric #-}
 
+-- Le module Literal définit la structure et les opérations pour les littéraux dans les formules logiques.
 module Literal
-  ( Literal(..),
-    neg,
-    toFormula
+  ( Literal(..),  -- Expose le type `Literal`
+    neg,          -- Fonction pour négocier un littéral
+    toFormula     -- Fonction pour convertir un littéral en une formule
   ) where
 
 import Data.Kind (Type)
 import GHC.Generics (Generic)
-import Formula (Formula(..))  -- Supposons que Formula a des constructeurs comme Var et Not
+import Formula (Formula(..))  -- Importe le type `Formula` d'un autre module, supposé avoir des constructeurs comme Var et Not
 
+-- | Type de données `Literal` avec des constructeurs pour les littéraux positifs et négatifs.
+-- Utilise GADTs pour plus de flexibilité et de sécurité de type.
 data Literal :: Type where
-  Positive :: String -> Literal
-  Negative :: String -> Literal
-  deriving (Eq, Ord, Show, Generic)
+  Positive :: String -> Literal  -- ^ Constructeur pour les littéraux positifs
+  Negative :: String -> Literal  -- ^ Constructeur pour les littéraux négatifs
+  deriving (Eq, Ord, Show, Generic)  -- Les instances courantes sont dérivées pour `Literal`.
 
+-- | La fonction 'neg' inverse la polarité d'un littéral.
+-- Si c'est positif, cela devient négatif et vice versa.
 neg :: Literal -> Literal
-neg (Positive s) = Negative s
-neg (Negative s) = Positive s
+neg (Positive s) = Negative s  -- Inverse un littéral positif
+neg (Negative s) = Positive s  -- Inverse un littéral négatif
 
+-- | La fonction 'toFormula' convertit un littéral en une formule logique.
+-- Les littéraux positifs deviennent des variables, les négatifs deviennent la négation des variables.
 toFormula :: Literal -> Formula
-toFormula (Positive s) = Var s          -- Supposons que Var est un constructeur de Formula
-toFormula (Negative s) = Not (Var s)    -- Supposons que Not est un constructeur de Formula
+toFormula (Positive s) = Var s          -- Convertit un littéral positif en une variable de formule
+toFormula (Negative s) = Not (Var s)    -- Convertit un littéral négatif en négation de variable de formule
