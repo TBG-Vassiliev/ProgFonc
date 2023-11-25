17c5d37b099a1ba40dd7d5df500d25cc9f273411
TBG-Vassiliev <guibertu5bgpe11314@gmail.com>
2023-11-25 11:31:08 +0100

    Create Main.hs

---
+++ src/Main.hs
@@ -0,0 +1,33 @@
+module Main (main) where
+
+import Formula
+import qualified Formula as F
+import Literal
+import qualified Literal as L
+import NormalForm
+import qualified NormalForm as NF
+
+complexFormula :: Formula
+complexFormula =
+  let
+    a = Var "A"
+    b = Var "B"
+    c = Var "C"
+    d = Var "D"
+    e = Var "E"
+    f = Var "F"
+    g = Var "G"
+  in
+    Or
+      (And a (Not b))
+      (Or
+        (And (Not c) d)
+        (And e (Or f (Not g)))
+      )
+    
+
+
+main :: IO ()
+main = do
+    print complexFormula
+    print (F.tautology(complexFormula))
