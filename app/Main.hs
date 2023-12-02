7ede524a647a32e7ee26af8dfe3b7fe27734722f
TBG-Vassiliev <guibertu5bgpe11314@gmail.com>
2023-12-02 20:24:03 +0100

    Changement de la formule dans le app/Main

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import qualified Data.Set as Set
 main :: IO ()
 main = do
     -- Entrer la formule
-    let formula = Or (Var "A") (Not (Var "A")) -- A ∨ ¬A
+    let formula = Or (And (Var "A") (Var "B")) (And (Not (Var "C")) (Or (Var "A") (Var "B"))) -- (A∧B)∨(¬C∧(A∨B))
 
     -- Affichage des formules
     putStrLn $ "Formule : " ++ show formula
