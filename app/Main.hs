064135482a5eb3c05f90e96bb335c9bbe3f3c01f
TBG-Vassiliev <guibertu5bgpe11314@gmail.com>
2023-12-02 12:36:09 +0100

    Update du Main

--- app/Main.hs
+++ app/Main.hs
@@ -3,45 +3,46 @@ import qualified NormalForm as NF
 import qualified Literal as Lit
 import qualified Data.Set as Set
 
+
+-- Obtenir une formule fourni par l'utilisateur, non vérifiée par le programme
+getFormula :: IO Formula
+getFormula = do
+    putStrLn "Veuillez entrer une formule logique :"
+    formuleString <- getLine
+    let formule = fromString formuleString
+    return formule
+
+
 main :: IO ()
 main = do
-    -- Création de littéraux et de formules
-    let litA = Lit.PosVar "A"
-    let litB = Lit.NegVar "B"
-    let form1 = Var "A"
-    let form2 = Not (Var "B")
-    let form3 = And form1 form2
-    let form4 = Or form1 (Var "B")
+    -- Lire l'entrée de l'utilisateur
+    formula <- getFormula
 
     -- Affichage des formules
-    putStrLn $ "Formule 1: " ++ show form1
-    putStrLn $ "Formule 2: " ++ show form2
-    putStrLn $ "Formule 3: " ++ show form3
-    putStrLn $ "Formule 4: " ++ show form4
+    putStrLn $ "Formule : " ++ show formula
 
     -- Simplification d'une formule
-    --let simplifiedForm = simplify form3
-    -- putStrLn $ "Formule simplifiée: " ++ show simplifiedForm
+    let simplifiedForm = simplify formula
+    putStrLn $ "Formule simplifiée : " ++ show simplifiedForm
     
-    -- Conversion d'une formule en CNF et inversement
-    let cnf = NF.fromFormula form4
-    putStrLn $ "CNF de form4: " ++ show cnf
+    -- -- Conversion d'une formule en CNF et inversement
+    let cnf = NF.fromFormula formula
+    putStrLn $ "CNF de formula : " ++ show cnf
+
     let cnfToFormula = NF.toFormula cnf
     putStrLn $ "Conversion CNF en Formule: " ++ show cnfToFormula
     
-    -- Vérification de tautologie
-    let isTautologyForm1 = tautology form1
-    putStrLn $ "form1 est-elle une tautologie? " ++ show isTautologyForm1
+    -- -- Vérification de tautologie
+    let isTautology = tautology formula
+    putStrLn $ "La formule entrée est-elle une tautologie ? " ++ show isTautology
 
-    -- Application de la règle de Robinson
+    -- -- Application de la règle de Robinson
     let robinsonApplied = NF.robinson cnf
     putStrLn $ "CNF après application de la règle de Robinson: " ++ show robinsonApplied
-    
+  
 
-
-
--- Pour supprimer les fichier de complations à la fin dans src/app :
--- rm Formula.o Formula.hi Literal.o Literal.hi NormalForm.o NormalForm.hi Main.o Main.hi haskell
+-- Pour supprimer les fichier de complations à la fin dans src :
+-- Executer le fichier clean.exe
 
 -- Pour compiler
 -- ghc -o haskell -i/home/cytech/Desktop/ProjetHaskell/src/ Main.hs
--- src/Main.hs
+++ src/Main.hs
@@ -1,33 +1,48 @@
-module Main (main) where
-
 import Formula
-import qualified Formula as F
-import Literal
-import qualified Literal as L
-import NormalForm
 import qualified NormalForm as NF
+import qualified Literal as Lit
+import qualified Data.Set as Set
 
-complexFormula :: Formula
-complexFormula =
-  let
-    a = Var "A"
-    b = Var "B"
-    c = Var "C"
-    d = Var "D"
-    e = Var "E"
-    f = Var "F"
-    g = Var "G"
-  in
-    Or
-      (And a (Not b))
-      (Or
-        (And (Not c) d)
-        (And e (Or f (Not g)))
-      )
-    
+
+-- Obtenir une formule fourni par l'utilisateur, non vérifiée par le programme
+getFormula :: IO Formula
+getFormula = do
+    putStrLn "Veuillez entrer une formule logique :"
+    formuleString <- getLine
+    let formule = fromString formuleString
+    return formule
 
 
 main :: IO ()
 main = do
-    print complexFormula
-    print (F.tautology(complexFormula))
+    -- Lire l'entrée de l'utilisateur
+    formula <- getFormula
+
+    -- Affichage des formules
+    putStrLn $ "Formule : " ++ show formula
+
+    -- Simplification d'une formule
+    let simplifiedForm = simplify formula
+    putStrLn $ "Formule simplifiée : " ++ show simplifiedForm
+    
+    -- -- Conversion d'une formule en CNF et inversement
+    let cnf = NF.fromFormula formula
+    putStrLn $ "CNF de formula : " ++ show cnf
+
+    let cnfToFormula = NF.toFormula cnf
+    putStrLn $ "Conversion CNF en Formule: " ++ show cnfToFormula
+    
+    -- -- Vérification de tautologie
+    let isTautology = tautology formula
+    putStrLn $ "La formule entrée est-elle une tautologie ? " ++ show isTautology
+
+    -- -- Application de la règle de Robinson
+    let robinsonApplied = NF.robinson cnf
+    putStrLn $ "CNF après application de la règle de Robinson: " ++ show robinsonApplied
+  
+
+-- Pour supprimer les fichier de complations à la fin dans src :
+-- Executer le fichier clean.exe
+
+-- Pour compiler
+-- ghc -o haskell -i/home/cytech/Desktop/ProjetHaskell/src/ Main.hs
