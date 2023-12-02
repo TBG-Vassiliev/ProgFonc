6ebd234140e3d136339238e4e5a635fa15c91da8
TBG-Vassiliev <guibertu5bgpe11314@gmail.com>
2023-12-02 20:21:37 +0100

    CNF FINALLY

--- src/NormalForm.hs
+++ src/NormalForm.hs
@@ -30,21 +30,58 @@ size (CNF clauses) = sum (map Set.size (Set.toList clauses))
 
 -- | Convert normal form to logical formula
 toFormula :: CNF -> Formula
-toFormula (CNF clauses) = foldr F.conj (F.fromBool True) (map toDisjunction (Set.toList clauses))
+toFormula (CNF clauses) = foldr F.And (F.fromBool True) (map toDisjunction (Set.toList clauses))
   where
     toDisjunction :: Set Literal -> Formula
-    toDisjunction literals = foldr F.disj (F.fromBool False) (map L.toFormula (Set.toList literals))
+    toDisjunction literals = foldr F.Or (F.fromBool False) (map L.toFormula (Set.toList literals))
 
 -- | Convert logical formula to normal form
 fromFormula :: Formula -> CNF
-fromFormula formula = CNF (Set.singleton (toLiterals formula))
-  where
-    toLiterals :: Formula -> Set Literal
-    toLiterals (F.Var var)     = Set.singleton (L.PosVar var)
-    toLiterals (F.Not (F.Var var)) = Set.singleton (L.NegVar var)
-    toLiterals (F.Or f1 f2)    = Set.union (toLiterals f1) (toLiterals f2)  
-    -- toLiterals (F.And f1 f2) = Set.fromList [ Set.fromList (Set.toList l1 ++ Set.toList l2) | l1 <- toLiterals f1, l2 <- toLiterals f2 ]
-    toLiterals _ = Set.empty
+fromFormula f =
+  let negativeDescent :: Formula -> Formula
+      negativeDescent (F.BoolConst b) = F.fromBool b
+      negativeDescent (F.Var v) = F.fromString v
+      negativeDescent (F.Not (F.BoolConst b)) = F.BoolConst (not b)
+      negativeDescent (F.Not (F.Var v)) = F.neg (F.fromString v)
+      negativeDescent (F.Not (F.Not f')) = negativeDescent f'
+      negativeDescent (F.Not (F.And f1 f2)) = F.Or (negativeDescent (F.Not f1)) (negativeDescent (F.Not f2))
+      negativeDescent (F.Not (F.Or f1 f2)) = F.And (negativeDescent (F.Not f1)) (negativeDescent (F.Not f2))
+      negativeDescent (F.And f1 f2) = F.And (negativeDescent f1) (negativeDescent f2)
+      negativeDescent (F.Or f1 f2) = F.Or (negativeDescent f1) (negativeDescent f2) in
+  
+  let jointure :: [[a]] -> [[a]] -> [[a]]
+      jointure [] ys = ys
+      jointure (x:xs) ys = map (x ++) ys ++ jointure xs ys in
+
+  let distribute :: CNF -> CNF -> CNF
+      distribute (CNF set1) (CNF set2) =
+        let mat1 = Set.toList $ Set.map Set.toList set1 in
+        let mat2 = Set.toList $ Set.map Set.toList set2 in
+          CNF $ Set.fromList $ map Set.fromList $ jointure mat1 mat2 in
+
+
+  let fromFormula' :: Formula -> CNF
+      fromFormula' f' = case f' of
+
+        F.BoolConst True -> CNF Set.empty
+        F.BoolConst False -> CNF $ Set.singleton Set.empty
+        F.Var v -> CNF $ Set.singleton $ Set.singleton $ L.fromPositive v
+        F.Not (F.BoolConst True) -> CNF $ Set.singleton Set.empty
+        F.Not (F.BoolConst False) -> CNF Set.empty
+        F.Not (F.Var v) -> CNF $ Set.singleton $ Set.singleton $ L.fromNegative v
+        F.Not (F.Not f'') -> fromFormula' f''
+        F.Not _ -> error "fromFormula: negative descent failed"
+        
+        F.And f1 f2 -> let CNF set1 = fromFormula' f1 in
+          let CNF set2 = fromFormula' f2 in
+            CNF (Set.union set1 set2)
+        
+        F.Or f1 f2 -> let CNF set1 = fromFormula' f1 in
+          let CNF set2 = fromFormula' f2 in
+            distribute (CNF set1) (CNF set2)
+      in
+
+    fromFormula' (negativeDescent f)
 
 -- | Apply ROBINSON's rule on clauses
 robinson :: CNF -> CNF
